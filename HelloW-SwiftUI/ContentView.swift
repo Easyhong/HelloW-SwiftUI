742751769263f4e570475d1c776e20020f6cb07b
easyhong <TaoZhangBoos@163.com>
2020-06-29 13:05:36 +0800

    Initial Commit

---
+++ HelloW-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloW-SwiftUI
+//
+//  Created by ronzhang on 2020/6/29.
+//  Copyright © 2020 ronzhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
